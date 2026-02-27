f1e8dccc909005797e11a08be236c429555e1d28
LEELAVENKATASAISUMANTHE <1by23cs111@bmsit.in>
2026-02-27 19:10:58 +0530

    one

--- backend/main.js
+++ backend/main.js
@@ -1,1 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+import autoconsume from "./utils/autoconsume.js";
+import connectDB from "./db/connectDB.js";
 
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.listen(PORT, async () => {
+    console.log(`Server is running on port ${PORT}`);
+    await connectDB();
+    await autoconsume();
+});
