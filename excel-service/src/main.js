a92bda54d31443aba25ce46b7cb7860d740141a2
nakul <nakul123426@gmail.com>
2026-03-30 22:05:30 +0530

    seperated excel into a different backend

---
+++ excel-service/src/main.js
@@ -0,0 +1,29 @@
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import { connectMongo } from "./db/mongo.js";
+import excelRoutes from "./excel/routes/excel.routes.js";
+import {
+  runExcelRetentionCleanup,
+  startExcelRetentionCleanupLoop,
+} from "./excel/services/excel.service.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = Number(process.env.PORT || 4001);
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/excel", excelRoutes);
+app.get("/", (_req, res) => {
+  res.send("Excel service is running");
+});
+
+app.listen(PORT, async () => {
+  console.log(`Excel service is running on port ${PORT}`);
+  await connectMongo();
+  await runExcelRetentionCleanup();
+  startExcelRetentionCleanupLoop();
+});
